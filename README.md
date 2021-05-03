808ad4f09800a91025f57cf20824080382437d03
BSpendlove <brandon-spendlove@hotmail.co.uk>
2021-05-03 10:36:50 +0000

    Added common TLVs and OrgSpecificTLV

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is a quick and dirty demo using Pythons socket and struct modules to export
 
 example.py will listen for all ethernet frames and will pass it into the LLDP object (server.py). The LLDP class performs a stupid check for the Ethernet EtherType to match 0x88cc (LLDP or SNAP header) and is currently listening on the IEEE defined multicast address `01:80:c2:00:00:0e`. There are other MAC destinations that LLDP implementations will use but this works perfectly fine for a lab with a Cisco IOS switch (or even IOSv/IOS XRv).
 
-The demo will only print out TLVs that I have decided to implement (3 mandatory TLVs which are 1, 2 and 3... however End of LLDPDU TLV should also be implemented as this is considered a mandatory TLV that should exist on all LLDP packets on the end of the message). Any non-implemented TLVs will just print the tlv-code, tlv-length and raw tlv-value encoded as the bytes.
+The demo will only print out TLVs that I have decided to implement. Any non-implemented TLVs will just print the tlv-code, tlv-length and raw tlv-value encoded as the bytes. OrgSpecific TLVs will capture the OUI and Org Subcode but will not perform any additional tasks other than just converting the raw bytes for the org defined string to a string.
 
 IEEE document for LLDP (802.1AB-2016 - IEEE Standard for Local and metropolitan area networks - Station and Media Access Control Connectivity Discovery) https://ieeexplore.ieee.org/document/7433915
 
@@ -13,20 +13,21 @@ Here is an example of some output connected to a Cisco 3750G-24T switch:
 {'ChassisID': {'chassis_id_subtype': 'MAC address', 'chassis_id': '00:16:c8:86:51:80'}}
 {'PortID': {'port_id_subtype': 'Interface name', 'port_id': 'Gi1/0/9'}}
 {'TimeToLive': 120}
-{'tlv-not-implemented': True, 'tlv-code': 5, 'tlv-length': 25, 'tlv-value': "b'csw01.brandonspendlove.co.uk'"}
-{'tlv-not-implemented': True, 'tlv-code': 6, 'tlv-length': 244, 'tlv-value': "b'Cisco IOS Software, C3750 Software (C3750-IPBASEK9-M), Version 12.2(55)SE12, RELEASE SOFTWARE (fc2)\\nTechnical Support: http://www.cisco.com/techsupport\\nCopyright (c) 1986-2017 by Cisco Systems, Inc.\\nCompiled Thu 28-Sep-17 02:29 by prod_rel_team'"}
-{'tlv-not-implemented': True, 'tlv-code': 4, 'tlv-length': 20, 'tlv-value': "b'GigabitEthernet1/0/9'"}
-{'tlv-not-implemented': True, 'tlv-code': 7, 'tlv-length': 4, 'tlv-value': "b'\\x00\\x14\\x00\\x14'"}
-{'tlv-not-implemented': True, 'tlv-code': 8, 'tlv-length': 12, 'tlv-value': "b'\\x05\\x01\\n\\x04\\x14\\x02\\x03\\x00\\x00\\x00\\xe0\\x00'"}
-{'tlv-not-implemented': True, 'tlv-code': 127, 'tlv-length': 7, 'tlv-value': 'b"\\x00\\x12\\xbb\\x01\\x00\'\\x04"'}
-{'tlv-not-implemented': True, 'tlv-code': 127, 'tlv-length': 33, 'tlv-value': "b'\\x00\\x12\\xbb\\x05WS-C3750G-24T (PowerPC405):L0'"}
-{'tlv-not-implemented': True, 'tlv-code': 127, 'tlv-length': 16, 'tlv-value': "b'\\x00\\x12\\xbb\\x0712.2(55)SE12'"}
-{'tlv-not-implemented': True, 'tlv-code': 127, 'tlv-length': 23, 'tlv-value': "b'\\x00\\x12\\xbb\\tCisco Systems, Inc.'"}
-{'tlv-not-implemented': True, 'tlv-code': 127, 'tlv-length': 17, 'tlv-value': "b'\\x00\\x12\\xbb\\nWS-C3750G-24T'"}
-{'tlv-not-implemented': True, 'tlv-code': 127, 'tlv-length': 8, 'tlv-value': "b'\\x00\\x12\\xbb\\x02\\x01A\\xc5n'"}
-{'tlv-not-implemented': True, 'tlv-code': 127, 'tlv-length': 8, 'tlv-value': "b'\\x00\\x12\\xbb\\x02\\x02\\x80\\x00\\x00'"}
-{'tlv-not-implemented': True, 'tlv-code': 127, 'tlv-length': 9, 'tlv-value': "b'\\x00\\x12\\xbb\\x03\\x02\\x03\\x02  '"}
-{'tlv-not-implemented': True, 'tlv-code': 127, 'tlv-length': 6, 'tlv-value': "b'\\x00\\x80\\xc2\\x01\\x00\\xe0'"}
-{'tlv-not-implemented': True, 'tlv-code': 127, 'tlv-length': 9, 'tlv-value': "b'\\x00\\x12\\x0f\\x01\\x03l\\x01\\x00\\x1e'"}
-{'tlv-not-implemented': True, 'tlv-code': 0, 'tlv-length': 0, 'tlv-value': "b''"}
+{'SystemName': 'csw01.network.w17bs.co.uk'}
+{'SystemDescription': 'Cisco IOS Software, C3750 Software (C3750-IPBASEK9-M), Version 12.2(55)SE12, RELEASE SOFTWARE (fc2)\nTechnical Support: http://www.cisco.com/techsupport\nCopyright (c) 1986-2017 by Cisco Systems, Inc.\nCompiled Thu 28-Sep-17 02:29 by prod_rel_team'}
+{'PortDescription': 'GigabitEthernet1/0/9'}
+{'SystemCapabilities': {'system_capabilities': {'Other': False, 'Repeater': False, 'Bridge': True, 'WLAN access point': False, 'Router': True, 'Telephone': False, 'DOCSIS cable device': False, 'Station only': False}, 'enabled_capabilities': {'Other': False, 'Repeater': False, 'Bridge': True, 'WLAN access point': False, 'Router': True, 'Telephone': False, 'DOCSIS cable device': False, 'Station only': False}}}
+{'ManagementAddress': {'address_string_length': 5, 'address_subtype': 1, 'management_address': '10.4.20.2', 'interface_subtype': 'system port number', 'interface_number': 224, 'oid_length': 0, 'oid_string': None}}
+{'OrgSpecificTLV': {'oui': '00:12:bb', 'org_subcode': 1, 'tlv_raw_value': 'b"\\x00\'\\x04"'}}
+{'OrgSpecificTLV': {'oui': '00:12:bb', 'org_subcode': 5, 'tlv_raw_value': "b'WS-C3750G-24T (PowerPC405):L0'"}}
+{'OrgSpecificTLV': {'oui': '00:12:bb', 'org_subcode': 7, 'tlv_raw_value': "b'12.2(55)SE12'"}}
+{'OrgSpecificTLV': {'oui': '00:12:bb', 'org_subcode': 9, 'tlv_raw_value': "b'Cisco Systems, Inc.'"}}
+{'OrgSpecificTLV': {'oui': '00:12:bb', 'org_subcode': 10, 'tlv_raw_value': "b'WS-C3750G-24T'"}}
+{'OrgSpecificTLV': {'oui': '00:12:bb', 'org_subcode': 2, 'tlv_raw_value': "b'\\x01A\\xc5n'"}}
+{'OrgSpecificTLV': {'oui': '00:12:bb', 'org_subcode': 2, 'tlv_raw_value': "b'\\x02\\x80\\x00\\x00'"}}
+{'OrgSpecificTLV': {'oui': '00:12:bb', 'org_subcode': 3, 'tlv_raw_value': "b'\\x02\\x03\\x02  '"}}
+{'OrgSpecificTLV': {'oui': '00:80:c2', 'org_subcode': 1, 'tlv_raw_value': "b'\\x00\\xe0'"}}
+{'OrgSpecificTLV': {'oui': '00:12:0f', 'org_subcode': 1, 'tlv_raw_value': "b'\\x03l\\x01\\x00\\x1e'"}}
+{'tlv-not-implemented': True, 'tlv-code': 127, 'tlv-length': 9, 'tlv-value': "b'\\x00\\x12\\x0f\\x01\\x03l\\x01\\x00\\x1e'"} # Just an example prior to me implementing OrgSpecificTLVs
+{'EndOfLLDPDU': None}
 ```
--- tlvs.py
+++ tlvs.py
@@ -1,12 +1,17 @@
 from struct import unpack
+import ipaddress
 
 
 class TLV:
-    #   0                   1                   2                   3
-    #   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-    #   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    #   |     Type    |      Length     |     TLV Information String..  |
-    #   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    """The Port ID TLV is a mandatory TLV that identifies the port component of the MSAP identifier associated
+    with the transmitting LLDP agent.
+    0                   1                   2                   3
+    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |     Type    |      Length     |     TLV Information String..  |
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    """
+
     MIN_LENGTH = 0  # End Of LLDPDU TLV
     MAX_LENGTH = 511  # Excluding Type and Length fields
 
@@ -44,12 +49,43 @@ class TLV:
         return None
 
 
+class EndOfLLDPDU(TLV):
+    """The End Of LLDPDU TLV is a 2-octet, all-zero TLV that is used to mark the end of the TLV sequence in
+    LLDPDUs.
+
+    0                   1
+    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |     Type    |      Length     |
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    """
+
+    MIN_LENGTH = 0
+    MAX_LENGTH = 0
+
+    def __init__(self, type_code, length, value, name="EndOfLLDPDU"):
+        super().__init__(type_code=type_code, length=length, value=value, name=name)
+
+    def decode(self):
+        if len(self.value) < self.MIN_LENGTH or len(self.value) > self.MAX_LENGTH:
+            raise ValueError(
+                f"LLDP Value {self.name} expected length should be no greater than {self.MAX_LENGTH} bytes."
+            )
+
+        return {self.name: None}
+
+
 class ChassisID(TLV):
-    #   0                   1                   2                   3
-    #   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-    #   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    #   |     Type    |      Length     |Chassis ID sub.|   Chassis ID  |
-    #   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    """The Chassis ID TLV is a mandatory TLV that identifies the chassis containing the IEEE 802 LAN station
+    associated with the transmitting LLDP agent.
+
+    0                   1                   2                   3
+    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |     Type    |      Length     |Chassis ID sub.|   Chassis ID  |
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    """
+
     MIN_LENGTH = 1
     MAX_LENGTH = 256
 
@@ -94,11 +130,16 @@ class ChassisID(TLV):
 
 
 class PortID(TLV):
-    #   0                   1                   2                   3
-    #   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-    #   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    #   |     Type    |      Length     |port ID subtype|    port ID    |
-    #   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    """The Port ID TLV is a mandatory TLV that identifies the port component of the MSAP identifier associated
+    with the transmitting LLDP agent.
+
+    0                   1                   2                   3
+    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |     Type    |      Length     |port ID subtype|    port ID    |
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    """
+
     MIN_LENGTH = 1
     MAX_LENGTH = 255
 
@@ -141,11 +182,15 @@ class PortID(TLV):
 
 
 class TimeToLive(TLV):
-    #   0                   1                   2                   3
-    #   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-    #   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    #   |     Type    |      Length     |          time to live         |
-    #   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    """The Time To Live TLV indicates the number of seconds that the recipient LLDP agent is to regard the
+    information associated with this MSAP identifier to be valid.
+    0                   1                   2                   3
+    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |     Type    |      Length     |          time to live         |
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    """
+
     MIN_LENGTH = 2
     MAX_LENGTH = 2
 
@@ -162,7 +207,265 @@ class TimeToLive(TLV):
         return {self.name: time_to_live}
 
 
+class PortDescription(TLV):
+    """The Port Description TLV allows network management to advertise the IEEE 802 LAN station’s port
+    description.
+
+    0                   1                   2                   3
+    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |     Type    |      Length     |        port description       |
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    """
+
+    MIN_LENGTH = 0
+    MAX_LENGTH = 255
+
+    def __init__(self, type_code, length, value, name="PortDescription"):
+        super().__init__(type_code=type_code, length=length, value=value, name=name)
+
+    def decode(self):
+        if len(self.value) < self.MIN_LENGTH or len(self.value) > self.MAX_LENGTH:
+            raise ValueError(
+                f"LLDP Value {self.name} expected length should be no greater than {self.MAX_LENGTH} bytes."
+            )
+
+        port_description = unpack(f"!{self.length}s", self.value)[0]
+
+        return {self.name: port_description.decode("utf-8")}
+
+
+class SystemName(TLV):
+    """The System Name TLV allows network management to advertise the system’s assigned name.
+
+    0                   1                   2                   3
+    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |     Type    |      Length     |          system name          |
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    """
+
+    MIN_LENGTH = 0
+    MAX_LENGTH = 255
+
+    def __init__(self, type_code, length, value, name="SystemName"):
+        super().__init__(type_code=type_code, length=length, value=value, name=name)
+
+    def decode(self):
+        if len(self.value) < self.MIN_LENGTH or len(self.value) > self.MAX_LENGTH:
+            raise ValueError(
+                f"LLDP Value {self.name} expected length should be no greater than {self.MAX_LENGTH} bytes."
+            )
+
+        system_name = unpack(f"!{self.length}s", self.value)[0]
+
+        return {self.name: system_name.decode("utf-8")}
+
+
+class SystemDescription(TLV):
+    """The System Description TLV allows network management to advertise the system’s description.
+
+    0                   1                   2                   3
+    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |     Type    |      Length     |       system description      |
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    """
+
+    MIN_LENGTH = 0
+    MAX_LENGTH = 255
+
+    def __init__(self, type_code, length, value, name="SystemDescription"):
+        super().__init__(type_code=type_code, length=length, value=value, name=name)
+
+    def decode(self):
+        if len(self.value) < self.MIN_LENGTH or len(self.value) > self.MAX_LENGTH:
+            raise ValueError(
+                f"LLDP Value {self.name} expected length should be no greater than {self.MAX_LENGTH} bytes."
+            )
+
+        system_description = unpack(f"!{self.length}s", self.value)[0]
+
+        return {self.name: system_description.decode("utf-8")}
+
+
+class SystemCapabilities(TLV):
+    """The System Capabilities TLV is an optional TLV that identifies the primary function(s) of the system and
+    whether or not these primary functions are enabled.
+
+    0                   1                   2                   3
+    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |     Type    |      Length     |system capabil.| enabled capab.|
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    """
+
+    MIN_LENGTH = 4
+    MAX_LENGTH = 4
+
+    CAPABILITIES_BITMAP = {
+        0: "Other",
+        1: "Repeater",
+        2: "Bridge",
+        3: "WLAN access point",
+        4: "Router",
+        5: "Telephone",
+        6: "DOCSIS cable device",
+        7: "Station only",
+    }
+
+    def __init__(self, type_code, length, value, name="SystemCapabilities"):
+        super().__init__(type_code=type_code, length=length, value=value, name=name)
+
+    def decode(self):
+        if len(self.value) < self.MIN_LENGTH or len(self.value) > self.MAX_LENGTH:
+            raise ValueError(
+                f"LLDP Value {self.name} expected length should be no greater than {self.MAX_LENGTH} bytes."
+            )
+
+        system_capabilities, enabled_capabilites = unpack("!HH", self.value)
+        system_capabilities = reversed(
+            [char for char in f"{system_capabilities:#010b}"[2:]]
+        )
+        enabled_capabilites = reversed(
+            [char for char in f"{enabled_capabilites:#010b}"[2:]]
+        )
+
+        results = {"system_capabilities": {}, "enabled_capabilities": {}}
+
+        for index, (s_capability, e_capability) in enumerate(
+            zip(system_capabilities, enabled_capabilites)
+        ):
+            results["system_capabilities"].update(
+                {self.CAPABILITIES_BITMAP[index]: (int(s_capability) == 1)}
+            )
+            results["enabled_capabilities"].update(
+                {self.CAPABILITIES_BITMAP[index]: (int(e_capability) == 1)}
+            )
+        return {self.name: results}
+
+
+class ManagementAddress(TLV):
+    """The Management Address TLV identifies an address associated with the local LLDP agent that may be used
+    to reach higher layer entities to assist discovery by network management.
+
+    0                   1                   2                   3
+    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |     Type    |      Length     |management add.| management ad.|
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    | management ad.| interface num.|        interface number       |
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |                               | OID string le.| object identi.|
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    """
+
+    MIN_LENGTH = 9
+    MAX_LENGTH = 167
+
+    def __init__(self, type_code, length, value, name="ManagementAddress"):
+        super().__init__(type_code=type_code, length=length, value=value, name=name)
+
+    def decode(self):
+        if len(self.value) < self.MIN_LENGTH or len(self.value) > self.MAX_LENGTH:
+            raise ValueError(
+                f"LLDP Value {self.name} expected length should be no greater than {self.MAX_LENGTH} bytes."
+            )
+
+        mgmt_addr_str_len = unpack("!B", self.value[:1])[
+            0
+        ]  # Length of Address Subtype + Address String
+        mgmt_addr_subtype, mgmt_addr = unpack(
+            f"!B{mgmt_addr_str_len - 1}s", self.value[1 : mgmt_addr_str_len + 1]
+        )
+
+        if mgmt_addr_subtype == 1:  # IPv4
+            mgmt_addr = ipaddress.IPv4Address(mgmt_addr)
+        elif mgmt_addr_subtype == 2:  # IPv6
+            mgmt_addr = ipaddress.IPv6Address(mgmt_addr)
+        else:  # Who cares...?
+            pass
+
+        interface_subtypes = {
+            0: "Reserved",
+            1: "Unknown",
+            2: "ifIndex",
+            3: "system port number",
+        }
+
+        interface_subtype, interface = unpack(
+            "!Bl", self.value[mgmt_addr_str_len + 1 : mgmt_addr_str_len + 6]
+        )
+
+        if interface_subtype == 0 and interface > 0:
+            raise ValueError(
+                "Interface Index field must be 0 when interface_subtype field is Unknown. Bad implemention on vendor side..."
+            )
+
+        interface_subtype = interface_subtypes[interface_subtype]
+        oid_length = unpack(
+            "!B", self.value[mgmt_addr_str_len + 6 : mgmt_addr_str_len + 7]
+        )[0]
+        oid_string = None
+        if oid_length:
+            oid_string = unpack(
+                f"!{oid_length}s",
+                self.value[mgmt_addr_str_len + 7 : mgmt_addr_str_len + 7 + oid_length],
+            )
+
+        return {
+            self.name: {
+                "address_string_length": mgmt_addr_str_len,
+                "address_subtype": mgmt_addr_subtype,
+                "management_address": str(mgmt_addr),
+                "interface_subtype": interface_subtype,
+                "interface_number": interface,
+                "oid_length": oid_length,
+                "oid_string": oid_string if not oid_string else str(oid_string),
+            }
+        }
+
+class OrgSpecificTLV(TLV):
+    """The Management Address TLV identifies an address associated with the local LLDP agent that may be used
+    to reach higher layer entities to assist discovery by network management.
+
+    0                   1                   2                   3
+    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |     Type    |      Length     |management add.| management ad.|
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    | management ad.| interface num.|        interface number       |
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |                               | OID string le.| object identi.|
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    """
+
+    MIN_LENGTH = 4
+    MAX_LENGTH = 511
+
+    def __init__(self, type_code, length, value, name="OrgSpecificTLV"):
+        super().__init__(type_code=type_code, length=length, value=value, name=name)
+
+    def decode(self):
+        if len(self.value) < self.MIN_LENGTH or len(self.value) > self.MAX_LENGTH:
+            raise ValueError(
+                f"LLDP Value {self.name} expected length should be no greater than {self.MAX_LENGTH} bytes."
+            )
+
+        oui, org_subtype = unpack("!3sB", self.value[:4])
+
+        return {
+            self.name : {
+                "oui": oui.hex(":"),
+                "org_subcode": org_subtype,
+                "tlv_raw_value": str(self.value[4:])
+            }
+        }
+
+
 class TLVNotImplemented(TLV):
+    """Generic class for TLVs not implemented or reserved."""
+
     def __init__(self, type_code, length, value, name="TLVNotImplementedInCode"):
         super().__init__(type_code=type_code, length=length, value=value, name=name)
 
@@ -175,4 +478,15 @@ class TLVNotImplemented(TLV):
         }
 
 
-TYPE_CODES = {1: ChassisID, 2: PortID, 3: TimeToLive}
+TYPE_CODES = {
+    0: EndOfLLDPDU,
+    1: ChassisID,
+    2: PortID,
+    3: TimeToLive,
+    4: PortDescription,
+    5: SystemName,
+    6: SystemDescription,
+    7: SystemCapabilities,
+    8: ManagementAddress,
+    127: OrgSpecificTLV
+}
